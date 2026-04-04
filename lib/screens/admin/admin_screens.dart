e3189c2a25830ac8b373a0fae9111a5905715840
natour101 <97555884+natour101@users.noreply.github.com>
2026-04-04 15:53:23 +0300

    قث

--- lib/screens/admin/admin_screens.dart
+++ lib/screens/admin/admin_screens.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' show DateFormat;
 
 import '../../controllers/auth_controller.dart';
 import '../../controllers/dashboard_controller.dart';
