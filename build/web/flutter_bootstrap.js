1676fa2bd902f7688cc25146166e5dfcfccf4126
natour101 <97555884+natour101@users.noreply.github.com>
2026-04-05 02:19:10 +0300

    ة

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1118336480" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1671924806" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
